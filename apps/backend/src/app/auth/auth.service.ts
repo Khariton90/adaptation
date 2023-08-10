b46d222e7676379308d9268082aa487f3eb17ef0
Khariton90 <82729640+Khariton90@users.noreply.github.com>
2023-08-11 00:19:19 +0300

    add password hash

--- apps/backend/src/app/auth/auth.service.ts
+++ apps/backend/src/app/auth/auth.service.ts
@@ -1,23 +1,29 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from "../users/users.service";
+import { UsersEntity } from "../users/users.entity";
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService
-  ) {}
+  ) { }
 
   async signIn(email: string, pass: string) {
     const user = await this.usersService.findByEmail(email);
+    const validPassword = await new UsersEntity(user).comparePassword(pass);
 
-    if (user.password !== pass) {
+    if (!validPassword) {
       throw new UnauthorizedException();
     }
 
-    const payload = {sub: user._id, email: user.email, role: user.userRole};
-    
+    const payload = {
+      sub: user._id,
+      email: user.email,
+      role: user.userRole
+    };
+
     return {
       access_token: await this.jwtService.signAsync(payload),
     }
